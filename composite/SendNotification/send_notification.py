d99693ab11383bf2f3f29a68fcf5812e647dfb00
Isaiah Chia <130481550+Isaidchia@users.noreply.github.com>
2025-03-27 15:50:33 +0800

    added acknowledgement email

--- composite/SendNotification/send_notification.py
+++ composite/SendNotification/send_notification.py
@@ -35,9 +35,9 @@ def handle_message(ch, method, properties, body):
         if len(parts) == 2 and parts[1] == "status":            
             print("Processing status request with routing_key: ", method.routing_key)
             process_delivery_status(message_dict, method.routing_key)
-        elif method.routing_key == "request.acknowledge":
-            print("Processing acknowledge request...")
-            # process_match_result(message_dict)
+        elif len(parts) == 2 and parts[1] == "acknowledge":
+            print("Processing acknowledgement request...")
+            process_acknowledgment_request(message_dict, method.routing_key)
         else:
             print("Unknown routing key.")
        # Acknowledge the message after successful processing.
@@ -146,8 +146,23 @@ def delivery_status_email(status):
     </html>
     """
     return plain_text, html_text
+def request_acknowledgement_email():
+    """
+    Returns a tuple of (plain_text, html_text)
+    incorporating the delivery status into the email body.
+    """
+    plain_text = f"Hello there! Your acknowledgement is required on GrabOrgan"
+    html_text = f"""
+    <html>
+      <body>
+        <h1>Hello there!</h1>
+        <p>Your <strong>acknowledgement</strong> is required on GrabOrgan</p>
+      </body>
+    </html>
+    """
+    return plain_text, html_text
 
-def create_status_email_message(driver_email, subject, plain_text, html_text):
+def create_email_message(driver_email, subject, plain_text, html_text):
     # Build the final email message dictionary.
     email_message = {
         "senderAddress": EMAIL_SUBDOMAIN,
@@ -207,8 +222,8 @@ def process_delivery_status(message_dict, routing_key):
     else:
         plain_text, html_text = delivery_status_email("Status Updated")
     
-    email_message = create_status_email_message(driver_email, subject, plain_text, html_text)
-    print("Sending Email with status: " + status_key)
+    email_message = create_email_message(driver_email, subject, plain_text, html_text)
+    print("Sending Email with status: " + routing_key)
     email_resp = invoke_http(AZURE_EMAIL_URL, method="POST",json=email_message)
     json_resp = json.dumps(email_resp)
     code = email_resp["code"]
@@ -223,6 +238,63 @@ def process_delivery_status(message_dict, routing_key):
             body=json_resp,
             properties=pika.BasicProperties(delivery_mode=2)  # make message persistent
         )
+    
+def process_acknowledgment_request(message_dict, routing_key):
+    """
+    Process an acknowledgement message and send an email notification.
+
+    routing_key: a string indicating the status, e.g.:
+      - request.acknowledge
+      - completed.acknowledge
+
+    amqp message example:
+    {
+        "driverId": "driver123",
+        "email": "example@gmail.com"
+    }
+    """
+    subject_prefix = "Acknowledgement Required: "
+    status_key = routing_key.split(".")[0]
+
+    # Extract information from the message
+    driver_id = message_dict.get("driverId", "Driver")
+    driver_email = message_dict.get("email")
+
+    subject_dict = {
+        "request": "Delivery Assigned",
+        "completed": "Delivery Completed",
+
+    }
+
+    if status_key in subject_dict:
+        subject = subject_prefix + subject_dict[status_key]
+        plain_text, html_text = request_acknowledgement_email()
+        email_message = create_email_message(driver_email, subject, plain_text, html_text)
+        print("Sending Email with status: " + routing_key)
+        email_resp = invoke_http(AZURE_EMAIL_URL, "POST", json=email_message)
+        json_resp = json.dumps(email_resp)
+        code = email_resp["code"]
+
+        if code not in range(200,300):
+            return jsonify({"code": code, "message": email_resp["message"]}), code
+        
+        print("Publishing message to with routing_key: ", "acknowledge.info")
+        channel.basic_publish(
+                exchange="activity_log_exchange",
+                routing_key="acknowledge.info",
+                body=json_resp,
+                properties=pika.BasicProperties(delivery_mode=2)  # make message persistent
+            )
+    else:
+        print("Publishing message to with routing_key: ", routing_key) # publish any unknown keys to the error log
+        channel.basic_publish(
+            exchange="error_handling_exchange",
+            routing_key="email.error",
+            body=message_dict,
+            properties=pika.BasicProperties(delivery_mode=2)  # make message persistent
+        )
+
+
 if __name__ == "__main__":
     print(f"This is {os.path.basename(__file__)} - Send Notification service...")
     consumer_thread = threading.Thread(target=run_async_consumer, daemon=True)
